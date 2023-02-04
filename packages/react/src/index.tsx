42adbbd98cd498c94a962323bbc8dd5728461aec
Ravi Souza <perfectbase@users.noreply.github.com>
2023-02-04 10:43:35 +0900

    add upload functionality

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -32,13 +32,21 @@ export const EdgeStoreProvider: React.FC<{ children: React.ReactNode }> = ({
     { onProgressChange } = {}
   ) => {
     try {
-      // TODO: implement
-      console.log("uploadImage");
-      onProgressChange?.(0);
-      for (let i = 0; i < 20; i++) {
-        await new Promise((resolve) => setTimeout(resolve, 300));
-        onProgressChange?.((100 / 20) * i);
-      }
+      const res = await fetch(
+        "https://qdnxe91xjc.execute-api.us-east-1.amazonaws.com/dev/request-upload",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            fileName: file.name,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const { signedUrl } = await res.json();
+      // Upload the file to the signed URL and get the progress
+      await uploadFile(file, signedUrl, onProgressChange);
     } finally {
       onProgressChange?.(100);
     }
@@ -77,4 +85,25 @@ export const useEdgeStore = () => {
   }
 
   return value;
+};
+
+const uploadFile = async (
+  file: File,
+  signedUrl: string,
+  onProgressChange?: (progress: number) => void
+) => {
+  const request = new XMLHttpRequest();
+  request.open("PUT", signedUrl);
+  request.upload.addEventListener("progress", (e) => {
+    if (e.lengthComputable) {
+      onProgressChange?.((e.loaded / e.total) * 100);
+    }
+  });
+  request.addEventListener("load", () => {
+    onProgressChange?.(100);
+  });
+  request.addEventListener("error", () => {
+    throw new Error("Error uploading file");
+  });
+  request.send(file);
 };
