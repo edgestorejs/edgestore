cc8dadfe25c60aeb39f1debd46c178788dca0bdb
Ravi Souza <perfectbase@users.noreply.github.com>
2023-03-19 15:50:14 +0900

    implement the AWS provider

--- packages/react/src/providers/AWSProvider.ts
+++ packages/react/src/providers/AWSProvider.ts
@@ -1,32 +1,83 @@
+import {
+  PutObjectCommand,
+  HeadObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { Provider } from "./types";
+import { v4 as uuidv4 } from "uuid";
 
 export type AWSProviderOptions = {
-  accessKey?: string;
-  secretKey?: string;
+  accessKeyId?: string;
+  secretAccessKey?: string;
   region?: string;
   bucketName?: string;
 };
 
 export default function AWSProvider(options?: AWSProviderOptions): Provider {
   const {
-    accessKey = process.env.ES_AWS_ACCESS_KEY_ID,
-    secretKey = process.env.ES_AWS_SECRET_ACCESS_KEY,
+    accessKeyId = process.env.ES_AWS_ACCESS_KEY_ID,
+    secretAccessKey = process.env.ES_AWS_SECRET_ACCESS_KEY,
     region = process.env.ES_AWS_REGION,
     bucketName = process.env.ES_AWS_BUCKET_NAME,
   } = options || {};
+
+  const credentials =
+    accessKeyId && secretAccessKey
+      ? {
+          accessKeyId,
+          secretAccessKey,
+        }
+      : undefined;
+  const s3Client = new S3Client({ region, credentials });
+
+  const baseUrl =
+    process.env.EDGE_STORE_BASE_URL ||
+    `https://${bucketName}.s3.${region}.amazonaws.com`;
+
   return {
     init: async (req, res) => {
       res.json({
         token: "TODO",
-        baseUrl: "TODO",
+        baseUrl,
       });
     },
     requestUpload: async (req, res) => {
-      // TODO: Implement
+      console.log("requestUpload", req.body);
+      const fileName =
+        req.body.name ?? `${uuidv4()}.${req.body.extension.replace(".", "")}`;
+      const filePath = req.body.path ?? "/"; // TODO: handle public path
+      const fileKey = `${filePath}${fileName}`.replace(/^\//, "");
+
+      if (!req.body.overwrite) {
+        // check if file already exists
+        const command = new HeadObjectCommand({
+          Bucket: bucketName,
+          Key: fileKey,
+        });
+
+        try {
+          await s3Client.send(command);
+          throw new Error("File already exists");
+        } catch {
+          // file does not exist, continue
+        }
+      }
+      const command = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: fileKey,
+        ContentType: getMimeType(req.body.extension),
+      });
+
+      const signedUrl = await getSignedUrl(s3Client, command, {
+        expiresIn: 60 * 60, // 1 hour
+      });
+
+      const url = `${baseUrl}/${fileKey}`;
       res.json({
-        signedUrl: "TODO",
-        url: "TODO",
-        path: "TODO",
+        signedUrl,
+        url,
+        path: "/" + fileKey,
       });
     },
     requestAccess: (req, res) => {
@@ -37,3 +88,24 @@ export default function AWSProvider(options?: AWSProviderOptions): Provider {
     },
   };
 }
+
+const getMimeType = (extension: string) => {
+  const parsedExtension = extension.toLowerCase().replace(".", "");
+  switch (parsedExtension) {
+    case "png":
+      return "image/png";
+    case "jpg":
+    case "jpeg":
+      return "image/jpeg";
+    case "gif":
+      return "image/gif";
+    case "webp":
+      return "image/webp";
+    case "svg":
+      return "image/svg+xml";
+    case "bmp":
+      return "image/bmp";
+    default:
+      return "application/octet-stream";
+  }
+};
--- packages/react/src/providers/types.ts
+++ packages/react/src/providers/types.ts
@@ -1,9 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+export type RequestUploadBody = {
+  path?: string | undefined;
+  name?: string | undefined;
+  public?: boolean | undefined;
+  overwrite?: boolean | undefined;
+  extension: string;
+  size: number;
+};
+
 export type Provider = {
   init: (req: NextApiRequest, res: NextApiResponse) => void | Promise<void>;
   requestUpload: (
-    req: NextApiRequest,
+    req: Omit<NextApiRequest, "body"> & { body: RequestUploadBody },
     res: NextApiResponse
   ) => void | Promise<void>;
   requestAccess: (
