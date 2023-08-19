b2c9ceb001429e89568e19cd93f1587e58518d1e
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-08-19 22:28:20 +0900

    setup google analytics

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,6 +44,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-HRW63MNKJZ',
+        },
       }),
     ],
     [
