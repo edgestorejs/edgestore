92cd6ffea32c5507e6d0c7cf39a6a5455dee1fa7
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-08-28 17:59:51 +0900

    fix docs build error

---
+++ docs/src/pages/pricing.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Page() {
+  // this page is only to prevent a build error
+  // the actual pricing page will be diplayed through the vercel rewrite settings
+  return <div>Pricing placeholder</div>;
+}
