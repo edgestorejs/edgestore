d05ef0a6c021f849d36e88ea3af52d0b23f6a130
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-08-28 18:00:15 +0900

    fix typo

--- docs/src/pages/pricing.tsx
+++ docs/src/pages/pricing.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Page() {
   // this page is only to prevent a build error
-  // the actual pricing page will be diplayed through the vercel rewrite settings
+  // the actual pricing page will be displayed through the vercel rewrite settings
   return <div>Pricing placeholder</div>;
 }
