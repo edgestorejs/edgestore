cce4c57382eae2db713be3da9bfa09dc6e93cc67
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-09-27 20:29:59 +0900

    reset aws example

--- examples/next-basic-aws/src/pages/_app.tsx
+++ examples/next-basic-aws/src/pages/_app.tsx
@@ -1,11 +1,6 @@
-import '../styles/globals.css';
-import { EdgeStoreProvider } from '@edgestore/react';
-import type { AppProps } from 'next/app';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <EdgeStoreProvider>
-      <Component {...pageProps} />
-    </EdgeStoreProvider>
-  );
+  return <Component {...pageProps} />
 }
