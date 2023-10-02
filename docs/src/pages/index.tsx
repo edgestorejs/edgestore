98a7628f3a841eb2a9a9412a4ac98b29dc66bfab
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-10-02 17:42:49 +0900

    fix lp title

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,15 +1,13 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import React from 'react';
 import Homepage from '../components/Homepage';
 import { PageFrame } from '../components/PageFrame';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <div className="homepage">
       <Layout
-        title={`Easily handle file uploads in Next.js - ${siteConfig.title}`}
+        title={`Easily handle file uploads in Next.js`}
         description="The best developer experience for uploading files from your Next.js app."
       >
         <PageFrame>
