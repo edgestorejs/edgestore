6fe806f7fa39bada2ae252266e1165dad5d1e4a3
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-12-20 15:42:54 +0900

    update vite example

--- examples/vite-basic/src/App.tsx
+++ examples/vite-basic/src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <EdgeStoreProvider basePath="http://localhost:3001/edgestore">
       <div>
-        AAAAAA
         <UploadInput />
       </div>
     </EdgeStoreProvider>
@@ -18,5 +17,32 @@ function UploadInput() {
   const [file, setFile] = useState<File | null>(null);
   const { edgestore } = useEdgeStore();
 
-  return <div>UPLOAD</div>;
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          setFile(e.target.files?.[0] ?? null);
+        }}
+      />
+      <button
+        onClick={async () => {
+          if (file) {
+            const res = await edgestore.publicFiles.upload({
+              file,
+              onProgressChange: (progress) => {
+                // you can use this to show a progress bar
+                console.log(progress);
+              },
+            });
+            // you can run some server action or api here
+            // to add the necessary data to your database
+            console.log(res);
+          }
+        }}
+      >
+        Upload
+      </button>
+    </div>
+  );
 }
