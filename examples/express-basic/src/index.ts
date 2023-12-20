ec5b8312ced3cc05e1282202cce5cff052087a1f
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2023-12-20 15:43:28 +0900

    fix express example

--- examples/express-basic/src/index.ts
+++ examples/express-basic/src/index.ts
@@ -1,13 +1,41 @@
 import { initEdgeStore } from '@edgestore/server';
 import { createEdgeStoreExpressHandler } from '@edgestore/server/adapters/express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 
-const es = initEdgeStore.create();
+// --- EXPRESS CONFIG ---
+
+const PORT = process.env.PORT ?? 3001;
+
+const app = express();
 
 /**
- * This is the main router for the Edge Store buckets.
+ * Your express app is probably running in a different port than your frontend app.
+ * To avoid CORS issues, we should use the cors middleware.
  */
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  }),
+);
+/**
+ * Edge Store uses cookies to store the context token.
+ * We need to use the cookie parser middleware to parse the cookies.
+ */
+app.use(cookieParser());
+/**
+ * We need to have access to the json request body.
+ * We can use the body parser middleware to parse the request.
+ */
+app.use(bodyParser.json());
+
+// --- EDGESTORE ROUTER CONFIG ---
+
+const es = initEdgeStore.create();
+
 const edgeStoreRouter = es.router({
   publicFiles: es.fileBucket(),
 });
@@ -18,18 +46,16 @@ const handler = createEdgeStoreExpressHandler({
   router: edgeStoreRouter,
 });
 
-const app = express();
-app.use(cors());
-
-const PORT = process.env.PORT ?? 3001;
+// --- EXPRESS ROUTES ---
 
 app.get('/', (req, res) => {
   console.log(req), res.send('Hello from server!');
 });
 
+// set the get and post routes for the edgestore router
 app.get('/edgestore/*', handler);
 app.post('/edgestore/*', handler);
 
 app.listen(PORT, () => {
-  console.log(`⚡Server is running here 👉 https://localhost:${PORT}`);
+  console.log(`⚡Server is running here 👉 http://localhost:${PORT}`);
 });
