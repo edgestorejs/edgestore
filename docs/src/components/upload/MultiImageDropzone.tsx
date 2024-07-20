7b2fa79f4929fcb87654e69bc19eb60b8c25a50d
Ravi Souza <103035973+perfectbase@users.noreply.github.com>
2024-07-20 12:18:18 +0900

    change example components buttons type to "button"

--- docs/src/components/upload/MultiImageDropzone.tsx
+++ docs/src/components/upload/MultiImageDropzone.tsx
@@ -192,7 +192,9 @@ const MultiImageDropzone = React.forwardRef<HTMLInputElement, InputProps>(
                 <UploadCloudIcon className="mb-2 h-7 w-7" />
                 <div className="text-gray-400">drag & drop to upload</div>
                 <div className="mt-3">
-                  <Button disabled={disabled}>select</Button>
+                  <Button type="button" disabled={disabled}>
+                    select
+                  </Button>
                 </div>
               </div>
             </div>
