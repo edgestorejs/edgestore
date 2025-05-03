c0f1b127081c63493530887285698c5eb8169f7e
Ravi <103035973+perfectbase@users.noreply.github.com>
2025-05-03 10:47:49 +0900

    update fastify example

--- examples/fastify-basic/src/index.ts
+++ examples/fastify-basic/src/index.ts
@@ -32,24 +32,7 @@ async function configureApp() {
 const es = initEdgeStore.create();
 
 const edgeStoreRouter = es.router({
-  publicImages: es
-    .imageBucket()
-    /**
-     * return `true` to allow upload
-     * By default every upload from your app is allowed.
-     */
-    .beforeUpload(({ ctx, input, fileInfo }) => {
-      console.log('beforeUpload', ctx, input, fileInfo);
-      return true; // allow upload
-    })
-    /**
-     * return `true` to allow delete
-     * This function must be defined if you want to delete files directly from the client.
-     */
-    .beforeDelete(({ ctx, fileInfo }) => {
-      console.log('beforeDelete', ctx, fileInfo);
-      return true; // allow delete
-    }),
+  publicFiles: es.fileBucket(),
 });
 
 export type EdgeStoreRouter = typeof edgeStoreRouter;
