088c031cb48ee8d352cd31ab8125d65b133cdfbb
Ravi <103035973+perfectbase@users.noreply.github.com>
2026-01-12 16:41:26 +0900

    upgrade fumadocs

--- docs/src/lib/source.ts
+++ docs/src/lib/source.ts
@@ -1,16 +1,13 @@
-import { docs } from '@/.source';
-import {
-  loader,
-  type InferMetaType,
-  type InferPageType,
-} from 'fumadocs-core/source';
+import { loader, type InferPageType } from 'fumadocs-core/source';
+import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
+import { docs } from 'fumadocs-mdx:collections/server';
 
-// See https://fumadocs.vercel.app/docs/headless/source-api for more info
+// See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  plugins: [lucideIconsPlugin()],
 });
 
 export type Page = InferPageType<typeof source>;
-export type Meta = InferMetaType<typeof source>;
