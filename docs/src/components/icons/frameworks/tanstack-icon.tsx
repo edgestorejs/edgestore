10d5bca053b4192e3271b1c05958de55769987b4
Ravi <103035973+raviships@users.noreply.github.com>
2026-09-23 15:03:57 +0900

    docs: refresh supported framework logos

--- docs/src/components/icons/frameworks/tanstack-icon.tsx
+++ docs/src/components/icons/frameworks/tanstack-icon.tsx
@@ -1,365 +1,42 @@
 import * as React from 'react';
 import { IconBase, type IconProps } from '../icon-base';
 
+// Official artwork: https://tanstack.com/favicon-light.svg
 export const TanStackIcon = React.forwardRef<SVGSVGElement, IconProps>(
   (props, ref) => {
+    const gradientId = React.useId();
     return (
-      <IconBase ref={ref} viewBox="0 0 633 633" {...props}>
+      <IconBase ref={ref} viewBox="0 0 18 18" {...props}>
+        <rect
+          width="18"
+          height="18"
+          rx="4"
+          fill={`url(#${gradientId})`}
+          fillOpacity="0.99"
+        />
+        <path
+          d="M11.223 13.665C10.5529 13.665 10.1862 13.8488 9.89289 13.9958C9.63954 14.1227 9.43953 14.2229 8.99949 14.2229C8.55946 14.2229 8.35944 14.1227 8.10609 13.9958C7.81274 13.8488 7.44604 13.665 6.77599 13.665C6.10594 13.665 5.73925 13.8488 5.44589 13.9958C5.19254 14.1227 4.99252 14.2229 4.55249 14.2229V15.1984C5.22254 15.1984 5.58924 15.0146 5.88259 14.8677C6.13594 14.7407 6.33596 14.6405 6.77599 14.6405C7.21602 14.6405 7.41604 14.7407 7.66939 14.8677C7.96275 15.0146 8.32944 15.1984 8.99949 15.1984C9.66954 15.1984 10.0362 15.0146 10.3296 14.8677C10.5829 14.7407 10.783 14.6405 11.223 14.6405C11.663 14.6405 11.863 14.7407 12.1164 14.8677C12.4097 15.0146 12.7764 15.1984 13.4465 15.1984V14.2229C13.0065 14.2229 12.8064 14.1227 12.5531 13.9958C12.2597 13.8488 11.893 13.665 11.223 13.665Z"
+          fill="#171717"
+        />
+        <path
+          d="M12.5534 12.1082C12.26 11.9612 11.8933 11.7775 11.2233 11.7775C10.5532 11.7775 10.1865 11.9612 9.89316 12.1082C9.81648 12.1449 9.74648 12.1817 9.67314 12.2117C9.61647 12.1616 9.58313 12.0982 9.5798 12.0313L9.42312 6.80995L11.5433 8.72747C12.05 9.18513 12.82 8.59718 12.5067 7.98919C12.3333 7.65513 12.1 7.34445 11.8066 7.08054C11.3533 6.66964 10.8132 6.4191 10.2398 6.30886H12.83C13.5168 6.30886 13.6934 5.33674 13.0434 5.11292C12.6567 4.98263 12.2433 4.90914 11.8133 4.90914C11.0266 4.90914 10.2965 5.153 9.68647 5.5639L11.5433 3.88357C12.05 3.4259 11.5433 2.59743 10.9099 2.84798C10.5599 2.98828 10.2298 3.18872 9.93649 3.45597C9.47646 3.87355 9.16643 4.39802 8.99975 4.96927C8.83308 4.39802 8.52305 3.87355 8.06302 3.45597C7.76966 3.19206 7.43964 2.98828 7.08961 2.84798C6.45623 2.59743 5.94952 3.4259 6.45623 3.88357L8.31304 5.5639C7.70632 5.153 6.97627 4.90914 6.18621 4.90914C5.75618 4.90914 5.34281 4.97929 4.95612 5.11292C4.30607 5.3334 4.48608 6.30886 5.16947 6.30886H7.75966C7.18962 6.4191 6.64624 6.67298 6.19288 7.08054C5.89952 7.34445 5.66617 7.65179 5.49282 7.98919C5.17947 8.59384 5.94952 9.18179 6.45623 8.72747L8.54639 6.84002L8.38971 12.0347C8.38971 12.0982 8.35304 12.1583 8.30637 12.2084C8.2397 12.1783 8.17636 12.1483 8.10635 12.1115C7.813 11.9645 7.4463 11.7808 6.77625 11.7808C6.1062 11.7808 5.73951 11.9645 5.44615 12.1115C5.1928 12.2385 4.99279 12.3387 4.55275 12.3387V13.3141C5.2228 13.3141 5.5895 13.1304 5.88285 12.9834C6.13621 12.8565 6.33622 12.7563 6.77625 12.7563C7.21629 12.7563 7.4163 12.8565 7.66965 12.9834C7.96301 13.1304 8.3297 13.3141 8.99975 13.3141C9.66981 13.3141 10.0365 13.1304 10.3299 12.9834C10.5832 12.8565 10.7832 12.7563 11.2233 12.7563C11.6633 12.7563 11.8633 12.8565 12.1167 12.9834C12.41 13.1304 12.7767 13.3141 13.4468 13.3141V12.3387C13.0067 12.3387 12.8067 12.2385 12.5534 12.1115V12.1082Z"
+          fill="#171717"
+        />
         <defs>
-          <linearGradient id="b" x1="50%" x2="50%" y1="0%" y2="71.65%">
-            <stop offset="0%" stopColor="#6BDAFF" />
-            <stop offset="31.922%" stopColor="#F9FFB5" />
-            <stop offset="70.627%" stopColor="#FFA770" />
-            <stop offset="100%" stopColor="#FF7373" />
-          </linearGradient>
           <linearGradient
-            id="d"
-            x1="43.996%"
-            x2="53.441%"
-            y1="8.54%"
-            y2="93.872%"
+            id={gradientId}
+            x1="8"
+            y1="0"
+            x2="8"
+            y2="18"
+            gradientUnits="userSpaceOnUse"
           >
-            <stop offset="0%" stopColor="#673800" />
-            <stop offset="100%" stopColor="#B65E00" />
+            <stop stopColor="#FF5F5F" />
+            <stop offset="0.344449" stopColor="#FFA05C" />
+            <stop offset="0.733354" stopColor="#FFF27C" />
+            <stop offset="1" stopColor="#74DCFF" />
           </linearGradient>
-          <linearGradient id="e" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#2F8A00" />
-            <stop offset="100%" stopColor="#90FF57" />
-          </linearGradient>
-          <linearGradient id="f" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#2F8A00" />
-            <stop offset="100%" stopColor="#90FF57" />
-          </linearGradient>
-          <linearGradient id="g" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#2F8A00" />
-            <stop offset="100%" stopColor="#90FF57" />
-          </linearGradient>
-          <linearGradient id="h" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#2F8A00" />
-            <stop offset="100%" stopColor="#90FF57" />
-          </linearGradient>
-          <linearGradient id="i" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#2F8A00" />
-            <stop offset="100%" stopColor="#90FF57" />
-          </linearGradient>
-          <linearGradient id="j" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#2F8A00" />
-            <stop offset="100%" stopColor="#90FF57" />
-          </linearGradient>
-          <linearGradient
-            id="k"
-            x1="92.9%"
-            x2="8.641%"
-            y1="45.768%"
-            y2="54.892%"
-          >
-            <stop offset="0%" stopColor="#EE2700" />
-            <stop offset="100%" stopColor="#FF008E" />
-          </linearGradient>
-          <linearGradient
-            id="l"
-            x1="61.109%"
-            x2="43.717%"
-            y1="3.633%"
-            y2="43.072%"
-          >
-            <stop offset="0%" stopColor="#FFF400" />
-            <stop offset="100%" stopColor="#3C8700" />
-          </linearGradient>
-          <linearGradient id="m" x1="50%" x2="50%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#FFDF00" />
-            <stop offset="100%" stopColor="#FF9D00" />
-          </linearGradient>
-          <linearGradient
-            id="n"
-            x1="127.279%"
-            x2="0%"
-            y1="49.778%"
-            y2="50.222%"
-          >
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient
-            id="o"
-            x1="127.279%"
-            x2="0%"
-            y1="47.531%"
-            y2="52.469%"
-          >
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient
-            id="p"
-            x1="127.279%"
-            x2="0%"
-            y1="46.195%"
-            y2="53.805%"
-          >
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient id="q" x1="127.279%" x2="0%" y1="35.33%" y2="64.67%">
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient id="r" x1="127.279%" x2="0%" y1="4.875%" y2="95.125%">
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient id="s" x1="78.334%" x2="31.668%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient id="t" x1="57.913%" x2="44.88%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <linearGradient id="u" x1="50.495%" x2="49.68%" y1="0%" y2="100%">
-            <stop offset="0%" stopColor="#FFA400" />
-            <stop offset="100%" stopColor="#FF5E00" />
-          </linearGradient>
-          <circle id="a" cx={308.5} cy={308.5} r={308.5} />
-          <circle id="v" cx={307.5} cy={308.5} r={316.5} />
         </defs>
-        <g fill="none" fillRule="evenodd" transform="translate(9 8)">
-          <mask id="c" fill="#fff">
-            <use xlinkHref="#a" />
-          </mask>
-          <use xlinkHref="#a" fill="url(#b)" />
-          <ellipse
-            cx={81.5}
-            cy={602.5}
-            fill="#015064"
-            stroke="#00CFE2"
-            strokeWidth={25}
-            mask="url(#c)"
-            rx={214.5}
-            ry={185.968}
-          />
-          <ellipse
-            cx={535.5}
-            cy={602.5}
-            fill="#015064"
-            stroke="#00CFE2"
-            strokeWidth={25}
-            mask="url(#c)"
-            rx={214.5}
-            ry={185.968}
-          />
-          <ellipse
-            cx={81.5}
-            cy={640.5}
-            fill="#015064"
-            stroke="#00A8B8"
-            strokeWidth={25}
-            mask="url(#c)"
-            rx={214.5}
-            ry={185.968}
-          />
-          <ellipse
-            cx={535.5}
-            cy={640.5}
-            fill="#015064"
-            stroke="#00A8B8"
-            strokeWidth={25}
-            mask="url(#c)"
-            rx={214.5}
-            ry={185.968}
-          />
-          <ellipse
-            cx={81.5}
-            cy={676.5}
-            fill="#015064"
-            stroke="#007782"
-            strokeWidth={25}
-            mask="url(#c)"
-            rx={214.5}
-            ry={185.968}
-          />
-          <ellipse
-            cx={535.5}
-            cy={676.5}
-            fill="#015064"
-            stroke="#007782"
-            strokeWidth={25}
-            mask="url(#c)"
-            rx={214.5}
-            ry={185.968}
-          />
-          <g mask="url(#c)">
-            <path
-              fill="url(#d)"
-              stroke="#6E3A00"
-              strokeWidth={6.088}
-              d="M98.318 88.007c7.691 37.104 16.643 72.442 26.856 106.013 10.212 33.571 25.57 68.934 46.07 106.088l-51.903 11.67c-10.057-60.01-17.232-99.172-21.525-117.487-4.293-18.315-10.989-51.434-20.089-99.357l20.591-6.927"
-              transform="scale(-1 1) rotate(25 -300.37 -553.013)"
-            />
-            <g stroke="#2F8A00">
-              <path
-                fill="url(#e)"
-                strokeWidth={9.343}
-                d="M108.544 66.538s-13.54-21.305-37.417-27.785c-15.917-4.321-33.933.31-54.048 13.892C33.464 65.975 47.24 73.52 58.405 75.28c16.749 2.64 50.14-8.74 50.14-8.74Z"
-                transform="rotate(1 -6061.691 5926.397)"
-              />
-              <path
-                fill="url(#f)"
-                strokeWidth={9.343}
-                d="M108.544 67.138s-47.187-5.997-81.077 19.936C4.873 104.362-3.782 137.794 1.502 187.369c28.42-29.22 48.758-50.836 61.016-64.846 18.387-21.016 46.026-55.385 46.026-55.385Z"
-                transform="rotate(1 -6061.691 5926.397)"
-              />
-              <path
-                fill="url(#g)"
-                strokeWidth={9.343}
-                d="M108.544 66.538c-1.96-21.705 3.98-38.018 17.82-48.94C140.203 6.674 154.85.808 170.303 0c-4.865 21.527-12.373 36.314-22.524 44.361-10.151 8.048-23.23 15.44-39.236 22.177Z"
-                transform="rotate(1 -6061.691 5926.397)"
-              />
-              <path
-                fill="url(#h)"
-                strokeWidth={9.343}
-                d="M108.544 67.138c29.838-31.486 61.061-42.776 93.669-33.869C234.82 42.176 253.749 60.785 259 89.096c-34.898-3.657-59.974-6.343-75.228-8.058-15.254-1.716-40.33-6.349-75.228-13.9Z"
-                transform="rotate(1 -6061.691 5926.397)"
-              />
-              <path
-                fill="url(#i)"
-                strokeWidth={9.343}
-                d="M108.544 67.138c34.868-9.381 64.503-3.658 88.905 17.17 24.402 20.829 39.656 46.686 45.762 77.571-39.626-7.574-68.4-20.115-86.322-37.624a395.051 395.051 0 0 1-48.345-57.117Z"
-                transform="rotate(1 -6061.691 5926.397)"
-              />
-              <path
-                fill="url(#j)"
-                strokeWidth={9.343}
-                d="M108.544 67.138C76.206 82.6 57.608 105.366 52.75 135.436c-4.858 30.07-.292 62.89 13.698 98.462 24.873-41.418 38.905-71.368 42.096-89.849 3.191-18.48 3.191-44.118 0-76.91Z"
-                transform="rotate(1 -6061.691 5926.397)"
-              />
-              <path
-                strokeLinecap="round"
-                strokeWidth={5.91}
-                d="M211.284 173.477c-13.851 21.992-23.291 42.022-28.32 60.093-5.03 18.071-8.175 33.143-9.436 45.216"
-              />
-              <path
-                strokeLinecap="round"
-                strokeWidth={5.91}
-                d="M209.814 176.884c-23.982 2.565-42.792 10.469-56.428 23.714-13.639 13.245-23.483 26.136-29.536 38.674M219.045 167.299c29.028-7.723 50.972-10.173 65.831-7.352 14.859 2.822 26.807 7.659 35.842 14.51M211.31 172.618c20.29 9.106 38.353 19.052 54.186 29.837 15.833 10.786 27.714 20.99 35.643 30.617"
-              />
-            </g>
-            <path
-              stroke="#830305"
-              strokeLinecap="round"
-              strokeLinejoin="bevel"
-              strokeWidth={6.937}
-              d="m409.379 398.157-23.176 18.556M328.04 375.516l-22.313 28.398M312.904 353.698l53.18 59.816"
-            />
-            <path
-              fill="url(#k)"
-              d="M67.585 27.463H5.68C1.893 28.148 0 30.38 0 34.16c0 3.78 1.893 6.211 5.68 7.293h67.17l41.751-30.356c2.488-2.646 2.794-5.315.92-8.006s-4.6-3.626-8.177-2.803l-39.76 27.174Z"
-              transform="scale(-1 1) rotate(-9 2092.128 2856.854)"
-            />
-            <path
-              fill="#D8D8D8"
-              stroke="#FFF"
-              strokeLinecap="round"
-              strokeLinejoin="bevel"
-              strokeWidth={4.414}
-              d="m402.861 391.51.471-4.088M382.21 388.752l.472-4.087M361.546 385.404l.485-3.845M337.59 371.883l2.56-2.498M324.276 359.567l2.56-2.497"
-            />
-          </g>
-          <ellipse
-            cx={308.5}
-            cy={720.5}
-            fill="url(#l)"
-            mask="url(#c)"
-            rx={266}
-            ry={316.5}
-          />
-          <ellipse
-            cx={308.5}
-            cy={720.5}
-            stroke="#6DA300"
-            strokeOpacity={0.502}
-            strokeWidth={26}
-            mask="url(#c)"
-            rx={253}
-            ry={303.5}
-          />
-          <g mask="url(#c)">
-            <g transform="translate(389 -32)">
-              <circle cx={168.5} cy={113.5} r={113.5} fill="url(#m)" />
-              <circle
-                cx={168.5}
-                cy={113.5}
-                r={106}
-                stroke="#FFC900"
-                strokeOpacity={0.529}
-                strokeWidth={15}
-              />
-              <path
-                stroke="url(#n)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="M30 113H0"
-              />
-              <path
-                stroke="url(#o)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="M33.5 79.5 7 74"
-              />
-              <path
-                stroke="url(#p)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="m34 146-29 8"
-              />
-              <path
-                stroke="url(#q)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="m45 177-24 13"
-              />
-              <path
-                stroke="url(#r)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="m67 204-20 19"
-              />
-              <path
-                stroke="url(#s)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="m94.373 227-13.834 22.847"
-              />
-              <path
-                stroke="url(#t)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="M127.5 243.5 120 268"
-              />
-              <path
-                stroke="url(#u)"
-                strokeLinecap="round"
-                strokeLinejoin="bevel"
-                strokeWidth={12}
-                d="m167.5 252.5.5 24.5"
-              />
-            </g>
-          </g>
-          <circle
-            cx={307.5}
-            cy={308.5}
-            r={304}
-            stroke="#000"
-            strokeWidth={25}
-          />
-        </g>
       </IconBase>
     );
   },
